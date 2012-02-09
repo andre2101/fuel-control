2b4cf1d00a7c3e61bd6dacbea1d0ef10347150b8
Victor Fontes <contato@victorfontes.com>
2012-02-09 02:19:33 -0300

    select

--- fuelcontrol/fuels/views.py
+++ fuelcontrol/fuels/views.py
@@ -6,9 +6,10 @@ from php_simplex import get_fuel
 
 @render_to('index.html')
 def index(request):
+   	tanque = 50
    	marcas = CarMaker.objects.all()
-   	marca = request.GET.get('fabricante')
-   	carros = Car.objects.all()   	
+   	marca = CarMaker.objects.filter(name=request.GET.get('fabricante', 'Fiat'))
+   	carros = Car.objects.filter(car_maker=marca)   	
    	
    	if request.POST:
 		preco_alcool = float(request.POST.get('alcool'))	
@@ -16,19 +17,23 @@ def index(request):
 		
 		modelo = request.POST.get('modelo')	
 		modelo = Car.objects.get(pk=modelo)
-		tanque = 50
+		
 		urbano = get_fuel(preco_alcool, preco_gasolina, modelo.alc_urban, modelo.gas_urban, 325, tanque)
 		rodoviario = get_fuel(preco_alcool, preco_gasolina, modelo.alc_road, modelo.gas_road, 650, tanque)
-		print urbano
-		print rodoviario
+
 		URBANO = {
 			'km_alc': modelo.alc_urban * tanque,
 			'total_alc': preco_alcool * tanque,
 			
 			'km_gas': modelo.gas_urban * tanque,
-			'total_gas': preco_gasolina * tanque,
+			'total_gas': preco_gasolina * tanque,}
 
-		}
+		RODOVIARIO = {
+			'km_alc': modelo.alc_road * tanque,
+			'total_alc': preco_alcool * tanque,
+			
+			'km_gas': modelo.gas_road * tanque,
+			'total_gas': preco_gasolina * tanque,}
 
 		TEMPLATE='results.html'
 	return locals()
